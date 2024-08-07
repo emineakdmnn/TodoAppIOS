b5b742fe50cf82da90e0cc523dd644856301aba9
Emine Akduman <eakdmn@gmail.com>
2024-08-07 22:03:33 +0300

    first commit

---
+++ TodoAppIOS/Controller/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TodoAppIOS
+//
+//  Created by Emine Akduman on 7.08.2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Properties
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+       
+    }
+}
+
+ // MARK: - Helpers
+extension LoginViewController {
+    
+    private func style(){
+        view.backgroundColor = .red
+    }
+    
+    private func layout(){
+        
+    }
+}
+
